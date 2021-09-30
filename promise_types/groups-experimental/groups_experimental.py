aa6363042f55ded3ef7fcd0719ba79305054c5a0
Lars Erik Wik <lars.erik.wik@northern.tech>
2021-09-30 13:27:52 +0200

    Used black formatter on groups_experimental module
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- promise_types/groups-experimental/groups_experimental.py
+++ promise_types/groups-experimental/groups_experimental.py
@@ -10,7 +10,6 @@ class GroupsExperimentalPromiseTypeModule(PromiseModule):
         self._name_regex = re.compile(r"^[a-z_][a-z0-9_-]*[$]?$")
         self._name_maxlen = 32
 
-
     def validate_promise(self, promiser, attributes):
         # check promiser type
         if type(promiser) is not str:
@@ -18,11 +17,15 @@ class GroupsExperimentalPromiseTypeModule(PromiseModule):
 
         # check promiser value
         if self._name_regex.match(promiser) is None:
-            self.log_warning(f"Promiser groupname '{promiser}' should match regular expression '[a-z_][a-z0-9_-]*[$]?'")
+            self.log_warning(
+                f"Promiser groupname '{promiser}' should match regular expression '[a-z_][a-z0-9_-]*[$]?'"
+            )
 
         # check promiser length not too long
         if len(promiser) > self._name_maxlen:
-            raise ValidationError(f"Promiser '{promiser}' is too long: ({len(promiser)} > {self._name_maxlen})")
+            raise ValidationError(
+                f"Promiser '{promiser}' is too long: ({len(promiser)} > {self._name_maxlen})"
+            )
 
         # check attribute policy if present
         if "policy" in attributes:
@@ -30,18 +33,24 @@ class GroupsExperimentalPromiseTypeModule(PromiseModule):
 
             # check attribute policy type
             if type(policy) is not str:
-                raise ValidationError("Invalid type for attribute policy: expected string")
+                raise ValidationError(
+                    "Invalid type for attribute policy: expected string"
+                )
 
             # check attribute policy value
             if policy not in ("present", "absent"):
-                raise ValidationError(f"Invalid value '{policy}' for attribute policy: must be 'present' or 'absent'")
+                raise ValidationError(
+                    f"Invalid value '{policy}' for attribute policy: must be 'present' or 'absent'"
+                )
 
             # check attributes gid and members are not used with policy absent
             if policy == "absent":
                 if "gid" in attributes:
                     self.log_warning(f"Cannot assign gid to absent group '{promiser}'")
                 if "members" in attributes:
-                    self.log_warning(f"Cannot assign members to absent group '{promiser}'")
+                    self.log_warning(
+                        f"Cannot assign members to absent group '{promiser}'"
+                    )
 
         # check attribute gid if present
         if "gid" in attributes:
@@ -49,14 +58,18 @@ class GroupsExperimentalPromiseTypeModule(PromiseModule):
 
             # check attribute gid type
             if type(gid) not in (str, int):
-                raise ValidationError("Invalid type for attribute gid: expected string or int")
+                raise ValidationError(
+                    "Invalid type for attribute gid: expected string or int"
+                )
 
             # check attribute gid value
             if type(gid) == str:
-                try: 
+                try:
                     int(gid)
                 except ValueError:
-                    raise ValidationError(f"Invalid value '{gid}' for attribute gid: expected integer literal")
+                    raise ValidationError(
+                        f"Invalid value '{gid}' for attribute gid: expected integer literal"
+                    )
 
         # check attribute members if present
         if "members" in attributes:
@@ -66,19 +79,26 @@ class GroupsExperimentalPromiseTypeModule(PromiseModule):
 
             # check attribute only not used with attributes include or exclude
             if "only" in members and ("include" in members or "exclude" in members):
-                raise ValidationError("Attribute 'only' may not be used with attributes 'include' or 'exclude'")
-            
+                raise ValidationError(
+                    "Attribute 'only' may not be used with attributes 'include' or 'exclude'"
+                )
+
             # check attributes of attibutes in members
             for attr in members:
                 if attr not in ("only", "include", "exclude"):
-                    raise ValidationError(f"Invalid value '{attr}' in attribute members: must be 'only', 'exclude' or 'include'")
-            
+                    raise ValidationError(
+                        f"Invalid value '{attr}' in attribute members: must be 'only', 'exclude' or 'include'"
+                    )
+
             # make sure users aren't both included and excluded
             if "include" in members and "exclude" in members:
-                duplicates = set(members["include"]).intersection(set(members["exclude"]))
+                duplicates = set(members["include"]).intersection(
+                    set(members["exclude"])
+                )
                 if duplicates != set():
-                    raise ValidationError(f"Users {duplicates} both included and excluded from group '{promiser}'")
-
+                    raise ValidationError(
+                        f"Users {duplicates} both included and excluded from group '{promiser}'"
+                    )
 
     def evaluate_promise(self, promiser, attributes):
         # keep track of any repairs or failed repairs
@@ -92,7 +112,7 @@ class GroupsExperimentalPromiseTypeModule(PromiseModule):
         except GroupException as e:
             self.log_error(f"Failed to lookup group '{promiser}': {e}")
             failed_repairs += 1
-        
+
         # get promised gid if present
         promised_gid = attributes.get("gid")
 
@@ -134,18 +154,24 @@ class GroupsExperimentalPromiseTypeModule(PromiseModule):
         if group is not None:
             # check gid if present
             if promised_gid is not None and promised_gid != group.gid:
-                self.log_error(f"There is an existing group '{promiser}' with a different GID ({group.gid}) than promised ({promised_gid})")
+                self.log_error(
+                    f"There is an existing group '{promiser}' with a different GID ({group.gid}) than promised ({promised_gid})"
+                )
                 # We will not try to repair this, as this might grant permissions to group
                 failed_repairs += 1
-            
+
             # check members if present
             if "members" in attributes:
                 members = attributes["members"]
-                set_members_repairs, set_members_failed_repairs = self._set_members(group, members)
+                set_members_repairs, set_members_failed_repairs = self._set_members(
+                    group, members
+                )
                 repairs += set_members_repairs
                 failed_repairs += set_members_failed_repairs
 
-        self.log_debug(f"'{repairs}' repairs and '{failed_repairs}' failed repairs to promiser '{promiser}'")
+        self.log_debug(
+            f"'{repairs}' repairs and '{failed_repairs}' failed repairs to promiser '{promiser}'"
+        )
         if failed_repairs > 0:
             self.log_error(f"Promise '{promiser}' not kept")
             return Result.NOT_KEPT
@@ -157,7 +183,6 @@ class GroupsExperimentalPromiseTypeModule(PromiseModule):
         self.log_verbose(f"Promise '{promiser}' kept")
         return Result.KEPT
 
-
     def _set_members(self, group, members):
         repairs = 0
         failed_repairs = 0
@@ -165,25 +190,32 @@ class GroupsExperimentalPromiseTypeModule(PromiseModule):
         for attribute in members:
             if attribute == "include":
                 users = members["include"]
-                include_users_repairs, include_users_failed_repairs = self._include_users(group, users)
+                (
+                    include_users_repairs,
+                    include_users_failed_repairs,
+                ) = self._include_users(group, users)
                 repairs += include_users_repairs
                 failed_repairs += include_users_failed_repairs
 
             elif attribute == "exclude":
                 users = members["exclude"]
-                exclude_users_repairs, exclude_users_failed_repairs = self._exclude_users(group, users)
+                (
+                    exclude_users_repairs,
+                    exclude_users_failed_repairs,
+                ) = self._exclude_users(group, users)
                 repairs += exclude_users_repairs
                 failed_repairs += exclude_users_failed_repairs
 
             elif attribute == "only":
                 users = members["only"]
-                only_users_repairs, only_users_failed_repairs = self._only_users(group, users)
+                only_users_repairs, only_users_failed_repairs = self._only_users(
+                    group, users
+                )
                 repairs += only_users_repairs
                 failed_repairs += only_users_failed_repairs
 
         return repairs, failed_repairs
 
-    
     def _include_users(self, group, users):
         repairs = 0
         failed_repairs = 0
@@ -191,61 +223,75 @@ class GroupsExperimentalPromiseTypeModule(PromiseModule):
         for user in users:
             self.log_debug(f"User '{user}' should be included in group '{group.name}'")
             if user in group.members:
-                self.log_debug(f"User '{user}' already included in group '{group.name}'")
+                self.log_debug(
+                    f"User '{user}' already included in group '{group.name}'"
+                )
             else:
                 self.log_debug(f"User '{user}' not included in group '{group.name}'")
                 try:
                     group.add_member(user)
                 except GroupException as e:
-                    self.log_error(f"Failed to add user '{user}' to group '{group.name}': {e}")
+                    self.log_error(
+                        f"Failed to add user '{user}' to group '{group.name}': {e}"
+                    )
                     failed_repairs += 1
                 else:
                     self.log_info(f"Added user '{user}' to group '{group.name}'")
                     repairs += 1
-        
+
         return repairs, failed_repairs
 
-    
     def _exclude_users(self, group, users):
         repairs = 0
         failed_repairs = 0
 
         for user in users:
-            self.log_debug(f"User '{user}' should be excluded from group '{group.name}'")
+            self.log_debug(
+                f"User '{user}' should be excluded from group '{group.name}'"
+            )
             if user in group.members:
                 self.log_debug(f"User '{user}' not excluded from group '{group.name}'")
                 try:
                     group.remove_member(user)
                 except GroupException as e:
-                    self.log_error(f"Failed to remove user '{user}' from group '{group.name}': {e}")
+                    self.log_error(
+                        f"Failed to remove user '{user}' from group '{group.name}': {e}"
+                    )
                     failed_repairs += 1
                 else:
                     self.log_info(f"Removed user '{user}' from group '{group.name}'")
                     repairs += 1
             else:
-                self.log_debug(f"User '{user}' already excluded from group '{group.name}'")
+                self.log_debug(
+                    f"User '{user}' already excluded from group '{group.name}'"
+                )
 
         return repairs, failed_repairs
 
-    
     def _only_users(self, group, users):
         repairs = 0
         failed_repairs = 0
 
         self.log_debug(f"Group '{group.name}' should only contain members {users}")
-        if (set(users) != set(group.members)):
-            self.log_debug(f"Group '{group.name}' does not only contain members {users}")
+        if set(users) != set(group.members):
+            self.log_debug(
+                f"Group '{group.name}' does not only contain members {users}"
+            )
             try:
                 group.set_members(users)
             except GroupException as e:
-                self.log_error(f"Failed to set members of group '{group.name}' to only users {users}: {e}")
+                self.log_error(
+                    f"Failed to set members of group '{group.name}' to only users {users}: {e}"
+                )
                 failed_repairs += 1
             else:
-                self.log_info(f"Members of group '{group.name}' set to only users {users}")
+                self.log_info(
+                    f"Members of group '{group.name}' set to only users {users}"
+                )
                 repairs += 1
         else:
             self.log_debug(f"Group '{group.name}' does only contain members {users}")
-        
+
         return repairs, failed_repairs
 
 
@@ -260,87 +306,81 @@ class Group:
         self.gid = gid
         self.members = members
 
-    
     @staticmethod
     def lookup(group):
         try:
             with open("/etc/group") as f:
                 for line in f:
-                    if line.startswith(group + ':'):
-                        entry = line.strip().split(':')
+                    if line.startswith(group + ":"):
+                        entry = line.strip().split(":")
                         name = entry[0]
                         gid = entry[2]
-                        members = entry[3].split(',') if entry[3] else []
+                        members = entry[3].split(",") if entry[3] else []
                         return Group(name, gid, members)
         except Exception as e:
             raise GroupException(e)
         return None
-        
 
     @staticmethod
     def create(name, gid=None):
         command = ["groupadd", name]
-        if (gid):
+        if gid:
             command += ["--gid", gid]
         process = Popen(command, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = process.communicate()
+        _, stderr = process.communicate()
 
         if process.returncode != 0:
-            # we'll only use the first line of stderr output, 
+            # we'll only use the first line of stderr output,
             # as remaining lines dwell into too much detail
-            msg = stderr.decode("utf-8").strip().split('\n')[0]
+            msg = stderr.decode("utf-8").strip().split("\n")[0]
             raise GroupException(msg)
 
         return Group.lookup(name)
 
-
     def delete(self):
         command = ["groupdel", self.name]
         process = Popen(command, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = process.communicate()
+        _, stderr = process.communicate()
 
         if process.returncode != 0:
-            # we'll only use the first line of stderr output, 
+            # we'll only use the first line of stderr output,
             # as remaining lines dwell into too much detail
-            msg = stderr.decode("utf-8").strip().split('\n')[0]
+            msg = stderr.decode("utf-8").strip().split("\n")[0]
             raise GroupException(msg)
 
         return None
 
-
     def add_member(self, user):
         command = ["gpasswd", "--add", user, self.name]
         process = Popen(command, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = process.communicate()
+        _, stderr = process.communicate()
 
         if process.returncode != 0:
-            # we'll only use the first line of stderr output, 
+            # we'll only use the first line of stderr output,
             # as remaining lines dwell into too much detail
-            msg = stderr.decode("utf-8").strip().split('\n')[0]
+            msg = stderr.decode("utf-8").strip().split("\n")[0]
             raise GroupException(msg)
-
 
     def remove_member(self, user):
         command = ["gpasswd", "--delete", user, self.name]
         process = Popen(command, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = process.communicate()
+        _, stderr = process.communicate()
 
         if process.returncode != 0:
-            # we'll only use the first line of stderr output, 
+            # we'll only use the first line of stderr output,
             # as remaining lines dwell into too much detail
-            msg = stderr.decode("utf-8").strip().split('\n')[0]
+            msg = stderr.decode("utf-8").strip().split("\n")[0]
             raise GroupException(msg)
-
 
     def set_members(self, users):
         command = ["gpasswd", "--members", ",".join(users), self.name]
         process = Popen(command, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = process.communicate()
+        _, stderr = process.communicate()
 
         if process.returncode != 0:
-            # we'll only use the first line of stderr output, 
+            # we'll only use the first line of stderr output,
             # as remaining lines may dwell into too much detail
-            msg = stderr.decode("utf-8").strip().split('\n')[0]
+            msg = stderr.decode("utf-8").strip().split("\n")[0]
             raise GroupException(msg)
 
 
