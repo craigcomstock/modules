6b80185e088fbae5663e98e6bcd36ff27edc853c
Craig Comstock <craig.comstock@northern.tech>
2021-11-12 12:29:49 -0600

    sq

--- promise-types/ini/ini.py
+++ promise-types/ini/ini.py
@@ -1,3 +1,4 @@
+import io
 import sys
 
 from configparser import ConfigParser
@@ -10,10 +11,22 @@ class IniPromiseTypeModule(PromiseModule):
   def evaluate_promise(self, promiser, attributes):
     if not promiser.startswith("/"):
       raise ValidationError("File path '%s' must be absolute" % promiser)
+    self.log_info("promiser is '%s'" % promiser)
     config = ConfigParser(strict=False)
-    with open(promiser) as stream:
-      config.read_string("[top]\n" + stream.read())
-    self.log_info("config is 
+# TODO, determine if the file HAD a section or not, and write out accordingly later
+#    with open(promiser) as stream:
+#      config.read_string("[top]\n" + stream.read())
+    with open(promiser, 'a') as stream:
+      self.log_info("got the stream")
+      text=stream.read()
+      self.log_info("stream was '%s'" % text)
+      config.read_string(text)
+      output = io.StringIO()
+      config.write(output)
+      self.log_info("config is %s" % output.getvalue())
+      self.log_info("config is of type %s" % type(config))
+#      config.write(stream)
+
     return Result.KEPT;
 
 
